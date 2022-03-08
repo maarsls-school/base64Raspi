245a310df91797ff62b2076ffc6b0c476e20004e
Marcel Maffey <maffey.marcel@gmx.net>
2022-03-08 09:42:30 +0100

    changes

--- send.py
+++ send.py
@@ -3,7 +3,6 @@ from time import sleep
 from picamera import PiCamera
 import base64
 
-
 # Create an in-memory stream
 my_stream = BytesIO()
 camera = PiCamera()
@@ -11,5 +10,5 @@ camera.start_preview()
 # Camera warm-up time
 sleep(2)
 camera.capture(my_stream, 'jpeg')
-base64S = base64.b64decode(my_stream.getValue())
+base64S = base64.b64encode(my_stream.getValue())
 print(base64S)
