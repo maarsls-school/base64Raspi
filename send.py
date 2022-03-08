8c5471912cc8fb18a0f41f429c851df0d7f777e8
Marcel Maffey <maffey.marcel@gmx.net>
2022-03-08 09:54:27 +0100

    changes

--- send.py
+++ send.py
@@ -4,6 +4,21 @@ from picamera import PiCamera
 import base64
 from PIL import Image
 
+import base64
+import requests
+
+def encode_base64(fName):
+    with open(fName, 'rb') as file:
+        binary_file_data = file.read()
+        base64_encoded_data = base64.b64encode(binary_file_data)
+        return base64_encoded_data.decode('utf-8')
+
+def decode_Base64(fName, data):
+    data_base64 = data.encode('utf-8')
+    with open(fName, 'wb') as file:
+        decoded_data = base64.decodebytes(data_base64)
+        file.write(decoded_data)
+
 image = Image.new("RGB", (300, 50))
 # Create an in-memory stream
 my_stream = BytesIO()
@@ -15,3 +30,8 @@ camera.capture(my_stream, 'jpeg')
 image.save(my_stream, 'PNG')
 base64S = base64.b64encode(my_stream.getvalue()).decode()
 print(base64S)
+
+response = requests.get('http://ec2-3-66-167-52.eu-central-1.compute.amazonaws.com:5000/img_meta/3')
+res_json = response.json()
+print(res_json)
+decode_Base64('test_files/htl-logo-from-server3.png', res_json['data'])
